1ef5ae553cb018e6785b9a6f8e105d2ba0b4a998
sipra <sipramishra085@gmail.com>
2023-07-11 01:06:34 +0530

    [Sipra]Add:Find Integer maximum value

---
+++ GenericsAssignment/Maximum.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericsAssignment
+{
+    public class Maximum
+    {
+        public T Number<T>(T firstvalue, T secondvalue, T thirdvalue) where T : IComparable
+        {
+            //Case-1
+            if (firstvalue.CompareTo(secondvalue) > 0 && firstvalue.CompareTo(thirdvalue) > 0 ||
+            firstvalue.CompareTo(secondvalue) >= 0 && firstvalue.CompareTo(thirdvalue) > 0 ||
+                firstvalue.CompareTo(secondvalue) > 0 && firstvalue.CompareTo(thirdvalue) >= 0)
+            {
+                return firstvalue;
+            }
+            //Case-2
+            if (secondvalue.CompareTo(firstvalue) > 0 && secondvalue.CompareTo(thirdvalue) > 0 ||
+                secondvalue.CompareTo(firstvalue) >= 0 && secondvalue.CompareTo(thirdvalue) > 0 ||
+                secondvalue.CompareTo(firstvalue) > 0 && secondvalue.CompareTo(thirdvalue) >= 0)
+            {
+                return secondvalue;
+            }
+            //Case-3
+            if (thirdvalue.CompareTo(firstvalue) > 0 && thirdvalue.CompareTo(secondvalue) > 0 ||
+                thirdvalue.CompareTo(firstvalue) >= 0 && thirdvalue.CompareTo(secondvalue) > 0 ||
+                thirdvalue.CompareTo(firstvalue) > 0 && thirdvalue.CompareTo(secondvalue) >= 0)
+            {
+                return thirdvalue;
+            }
+            return firstvalue;
+        }
+
+    }
+}
---
+++ GenericsAssignment/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericsAssignment
+{
+    public  class Program
+    {
+        static void Main(string[] args)
+        {
+            Maximum maximum = new Maximum();
+            Console.WriteLine("Please Enter the FirstValue:");
+            var val = Console.ReadLine();
+            Console.WriteLine("Please Enter the SecondValue:");
+            var val_one = Console.ReadLine();
+            Console.WriteLine("Please Enter the ThirdValue:");
+            var val_two = Console.ReadLine();
+            var Value = maximum.Number(val, val_one, val_two);
+            Console.WriteLine("{0} is greater", Value);
+            Console.ReadLine();
+        }
+    }
+}
